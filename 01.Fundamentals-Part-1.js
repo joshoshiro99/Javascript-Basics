177f64a7dc75975475e864c538a8e6f57125f42b
Josh Oshiro <joshoshiro99@gmail.com>
2022-08-13 10:29:25 -0400

    Add practice fiiles for chapter 1

---
+++ 01.Fundamentals-Part-1.js
@@ -0,0 +1,23 @@
+console.log("Hello World!");
+
+//1
+console.log(20+37);
+
+//2
+console.log(1+2+3+4+5+6);
+
+//3
+console.log((4+6+9)/77);
+
+//4
+let a = 10;
+console.log(a);
+console.log(9*a);
+let b = 7 * 8;
+console.log(b);
+
+//5
+const MAX = 57;
+let actual = MAX -13;
+let percentage = actual/MAX;
+console.log(percentage);
