7fe6376d4000c6b0c678602660f2dbb574d4f8d6
Josh Oshiro <joshoshiro99@gmail.com>
2022-09-03 16:32:42 -0400

    Add js file for DOM manip exercise

---
+++ 10.DOM-Manipulation-and-Events/domManip.js
@@ -0,0 +1,7 @@
+const containter = document.querySelector('#containter');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text content!';
+
+container.appendChild(content);
